2f9cfc3eb83642457f189fec4e650f29dac29eea
abhishek1sahu <sahuabhi.sahu@gmail.com>
2025-08-17 18:04:24 +0530

    sec commit - AuditTrail API

--- obj/Debug/net8.0/AuditService.AssemblyInfo.cs
+++ obj/Debug/net8.0/AuditService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuditService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97463fede61775c993efc571ac0313432132e53a")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuditService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuditService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
